bbc742d823ac21ae478f8bc0b4a32c93e4cd2068
Xys-Tus <Sixtus Onyedibe>
2023-08-03 11:52:57 +0100

    Set up user authentication

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 
 app = Flask(__name__)
@@ -9,5 +12,14 @@ app.config['SECRET_KEY'] = 'ee6927c7fc78a6e79ac0d3fd1d4054f9'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 # to create a database instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+
+# Initializing our login Manager
+# it handles all the session in the background for us
+login_manager = LoginManager(app)
+# Login manager to set our login route and the 'login' is our login route function
+login_manager.login_view = 'login'
+# the info I think is bootstrap info class in our login alert
+login_manager.login_message_category = 'info'
 
 from flask_blog import routes
